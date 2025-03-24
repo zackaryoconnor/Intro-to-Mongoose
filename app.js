665d5c90ceeb5945f649913a531415d4af97256e
Zack OConnor <zackary25@gmail.com>
2025-03-24 10:01:20 -0400

    fixed a few bugs

--- app.js
+++ app.js
@@ -4,54 +4,24 @@ const mongoose = require(`mongoose`);
 const prompt = require('prompt-sync')();
 
 
-const Users = require(`./models/users`)
+const Users = require(`./models/users`);
 
 
 const connectToDatabase = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
-
-
-const usersChoice = prompt(`
-    What would you like to do?
-    
-        1. Create a customer
-        2. View all Customers
-        3. Update a customer
-        4. Delete a customer
-        5 quit
-    
-    Number of action to run:
-    `);
-    console.log(`User picked: ${usersChoice}`);
-
-    if (input === `1`) {
-        await createNewCustomer();
-    } else if (input === `2`) {
-        await viewAllCustomers();
-    } else if (input === `3`) {
-        await updateExistingCustomer();
-    } else if (input === `4`) {
-        await deleteExistingCustomer();
-    } else if (input === `5`) {
-        await quitProgram();
-    } else {
-        console.log("Invalid choice. Please select a valid option.");
-    }
-}
+};
 
 
 const welcomeLabel = `
- _ _ _ _ _ _ _ _ _ _ _ _ _    
-|                         |
-|    Welcome to the CRM   |
-|_ _ _ _ _ _ _ _ _ _ _ _ _|
+             _ _ _ _ _ _ _ _ _ _ _ _ _    
+            |                         |
+            |    Welcome to the CRM   |
+            |_ _ _ _ _ _ _ _ _ _ _ _ _|
 
 `;
 
 
-console.log(welcomeLabel)
-connectToDatabase();
-
+console.log(welcomeLabel);
 
 
 const createNewCustomer = async () => {
@@ -62,7 +32,6 @@ const createNewCustomer = async () => {
         age: age
     };
 
-    connectToDatabase();
     await Users.create(newCustomer);
     console.log(`Hello, ${newCustomer.name}! You have been successfully add to the CRM!`);
 };
@@ -71,7 +40,7 @@ const createNewCustomer = async () => {
 
 
 const viewAllCustomers = async () => {
-    await connectToDatabase();
+    
     const existingCustomers = await Users.find();
 
     console.log(`Below is a list of all the customers:`);
@@ -80,7 +49,7 @@ const viewAllCustomers = async () => {
     existingCustomers.forEach(customer => {
         console.log(`ID: ${customer.id}, Name: ${customer.name}, Age: ${customer.age}`);
     });
-}
+};
 
 
 
@@ -105,8 +74,7 @@ const updateExistingCustomer = async () => {
 
 
 const deleteExistingCustomer = async () => {
-    await connectToDatabase();
-    
+
     await viewAllCustomers();
 
     const selectedCustomer = prompt(`Copy and paste the id of the customer you would like to remove here:`);
@@ -118,9 +86,7 @@ const deleteExistingCustomer = async () => {
     } else {
         console.log(`No customer found with ID: ${selectedCustomer}.`);
     }
-
-    mongoose.connection.close();
-}
+};
 
 
 
@@ -128,9 +94,47 @@ const deleteExistingCustomer = async () => {
 const quitProgram = async () => {
     console.log(`Exiting...`);
     mongoose.connection.close();
-}
+};
 
 
+
+
+const runProgram = async () => {
+    await connectToDatabase()
+
+    while (true) {
+        const usersChoice = prompt(`
+            What would you like to do?
+            
+                1. Create a customer
+                2. View all Customers
+                3. Update a customer
+                4. Delete a customer
+                5 quit
+            
+            Number of action to run:
+            `);
+        
+            await connectToDatabase()
+        
+            if (usersChoice === `1`) {
+                await createNewCustomer();
+            } else if (usersChoice === `2`) {
+                await viewAllCustomers();
+            } else if (usersChoice === `3`) {
+                await updateExistingCustomer();
+            } else if (usersChoice === `4`) {
+                await deleteExistingCustomer();
+            } else if (usersChoice === `5`) {
+                await quitProgram();
+                break;
+            } else {
+                console.log("Invalid choice. Please select a valid option.");
+            };   
+    };
+};
+
+runProgram();
 
 
 // createNewCustomer();
